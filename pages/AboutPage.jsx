c3598f178bcce9aed042fe35172bd3ca97f061aa
hansfonfach <hans.fonfach@gmail.com>
2025-05-02 11:55:20 -0400

    se modificó la experiencia

--- pages/AboutPage.jsx
+++ pages/AboutPage.jsx
@@ -95,8 +95,7 @@ const aboutPage = () => {
                   </li>
                   <li className="mb-3 d-flex align-items-start">
                     <span>
-                      • 18 años de experiencia como directivo y relator en
-                      Educación y Coaching Ejecutivo.
+                      • 18 años de experiencia en cargos directivos y basta experiencia como relator y coach ejecutivo.
                     </span>
                   </li>
                   <li className="mb-3 d-flex align-items-start">
